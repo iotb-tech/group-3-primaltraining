ed576e3e1e04eff57551d9482299470e252df124
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-07-12 14:07:36 +0100

    feat: ensure responsiveness of the header section

--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <header>
       <nav>
@@ -28,28 +33,32 @@ const Header = () => {
 
             <ul className="hidden md:flex md:justify-center md:items-center md:gap-10">
               <li className="font-geist-mono text-black">
-                <a href="#home">HOME</a>
+                {/* <a href="#home">HOME</a> */}
+                <Link href="/" className="font-geist-mono text-black">
+                  HOME
+                </Link>
               </li>
+              <Link href="/about" className="font-geist-mono text-black">
+                ABOUT
+              </Link>
               <li className="font-geist-mono text-black">
-                <a href="#about">ABOUT</a>
-              </li>
-              <li className="font-geist-mono text-black">
-                <a href="#reserve-your-spot">
+                <Link href="/reserve-your-spot">
                   <button className="bg-[#808CFD] px-4 py-2 rounded-md cursor-pointer hover:bg-[#000000] hover:text-white">
                     RESERVE YOUR SPOT
                   </button>
-                </a>
+                </Link>
               </li>
             </ul>
 
             <button
               id="menu-btn"
+              onClick={toggleMenu}
               className="relative z-50 text-3xl font-light cursor-pointer md:hidden focus:outline-none"
             >
               <Image
                 id="menu-icon"
-                src="/icons/plus.svg"
-                alt="Plus Icon"
+                src={isOpen ? "/icons/minus.svg" : "/icons/plus.svg"}
+                alt={isOpen ? "Minus Icon" : "Plus Icon"}
                 width={32}
                 height={32}
                 className="w-8 h-8"
@@ -62,32 +71,37 @@ const Header = () => {
       {/* Mobile nav dropdown */}
       <div
         id="mobile-menu"
-        className="fixed top-[80px] left-0 z-40 hidden w-full flex-col justify-center bg-white border-b border-black md:hidden"
+        className={`fixed top-[80px] left-0 z-40 w-full flex-col justify-center bg-white border-b border-black md:hidden ${
+          isOpen ? "flex" : "hidden"
+        }`}
       >
         <ul className="flex flex-col text-black font-geist-mono">
           <li>
             <a
               href="#home"
-              className="flex h-[68px] items-center border-b border-black px-6 text-4xl"
+              onClick={() => setIsOpen(false)}
+              className="flex h-[68px] items-center border-b border-black px-6 text-2xl"
             >
               HOME
             </a>
           </li>
           <li>
-            <a
-              href="#about"
-              className="flex h-[68px] items-center border-b border-black px-6 text-4xl"
+            <Link
+              href="/about"
+              onClick={() => setIsOpen(false)}
+              className="flex h-[68px] items-center border-b border-black px-6 text-2xl"
             >
               ABOUT
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="#reserve-your-spot"
-              className="flex h-[76px] items-center bg-[#808CFD] px-6 text-4xl text-white"
+            <Link
+              href="/reserve-your-spot"
+              onClick={() => setIsOpen(false)}
+              className="flex h-[76px] items-center bg-[#808CFD] px-6 text-2xl text-white"
             >
               RESERVE YOUR SPOT
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
